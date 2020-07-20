8047602e2cc7e436a57a4eb58f50197ab6c3885b
Lassi Kortela <lassi@lassi.io>
2020-07-20 13:03:06 +0300

    Fix Gambit vector allocation
    On advice of Marc Feeley,
    
    - Allocate an object that the GC is free to relocate (___PSTATE)
      instead of a still, immovable object (NULL).
    
    - Check for a failed allocation and return #f in that case.

--- return-numeric-vector/s64vector-gambit.scm
+++ return-numeric-vector/s64vector-gambit.scm
@@ -3,7 +3,8 @@
     size_t n = 32;
     size_t nbytes = n * sizeof(___S64);
     size_t i;
-    ___SCMOBJ vector = ___EXT(___alloc_scmobj)(NULL, ___sS64VECTOR, nbytes);
+    ___SCMOBJ vector = ___EXT(___alloc_scmobj)(___PSTATE, ___sS64VECTOR, nbytes);
+    if (___FIXNUMP(vector)) ___return(___FAL);
     ___S64 *elements = ___CAST(___S64 *, ___BODY(vector));
     for (i = 0; i < n; i += 2) elements[i] = i;
     for (i = 1; i < n; i += 2) elements[i] = -i;

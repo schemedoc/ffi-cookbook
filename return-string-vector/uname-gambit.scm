fba9d2800dd30fceda41680360e3cab032b2aded
Lassi Kortela <lassi@lassi.io>
2020-08-21 11:24:13 +0300

    Properly use fixnums in Gambit VECTORSET

--- return-string-vector/uname-gambit.scm
+++ return-string-vector/uname-gambit.scm
@@ -9,19 +9,19 @@
     v = ___make_vector(___PSTATE, 5, ___FAL);
     ___NONNULLCHARSTRING_to_SCMOBJ(___PSTATE, names.sysname, &s,
                                    ___RETURN_POS);
-    ___VECTORSET(v, 0, s);
+    ___VECTORSET(v, ___FIX(0), s);
     ___NONNULLCHARSTRING_to_SCMOBJ(___PSTATE, names.nodename, &s,
                                    ___RETURN_POS);
-    ___VECTORSET(v, 4, s);
+    ___VECTORSET(v, ___FIX(1), s);
     ___NONNULLCHARSTRING_to_SCMOBJ(___PSTATE, names.release, &s,
                                    ___RETURN_POS);
-    ___VECTORSET(v, 8, s);
+    ___VECTORSET(v, ___FIX(2), s);
     ___NONNULLCHARSTRING_to_SCMOBJ(___PSTATE, names.version, &s,
                                    ___RETURN_POS);
-    ___VECTORSET(v, 12, s);
+    ___VECTORSET(v, ___FIX(3), s);
     ___NONNULLCHARSTRING_to_SCMOBJ(___PSTATE, names.machine, &s,
                                    ___RETURN_POS);
-    ___VECTORSET(v, 16, s);
+    ___VECTORSET(v, ___FIX(4), s);
     ___return(v);
 c-end
 ))

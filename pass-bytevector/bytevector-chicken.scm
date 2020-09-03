dbda6122fbb89e5290d8a85f65e396ad897146ae
Alex Arslan <ararslan@comcast.net>
2020-09-02 22:09:22 -0700

    Add Chicken bytevector to C example (#13)
    A Chicken port of Lassi's Gambit implementation.

---
+++ pass-bytevector/bytevector-chicken.scm
@@ -0,0 +1,21 @@
+(import (chicken foreign)
+        (srfi 4))
+
+(foreign-declare "#include <string.h>")
+
+(define (byte-after-first-null byte-vector)
+  ((foreign-lambda* scheme-object ([u8vector bytes]
+                                   [size_t n])
+                    "unsigned char* where = memchr(bytes, 0, n);
+                     if (where != NULL && where < bytes + n - 1) {
+                         C_return(C_fix(where[1]));
+                     } else {
+                         C_return(C_SCHEME_FALSE);
+                     }")
+   byte-vector (u8vector-length byte-vector)))
+
+(let ([bytes (make-u8vector 1024 1)])
+  (u8vector-set! bytes 1000 0)
+  (u8vector-set! bytes 1001 42)
+  (display (byte-after-first-null bytes))
+  (newline))

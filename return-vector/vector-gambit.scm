3da268e92eee4b122f4f2bb5fefa47715b900c6d
Lassi Kortela <lassi@lassi.io>
2020-10-11 16:29:43 +0300

    Refactor

--- return-vector/vector-gambit.scm
+++ return-vector/vector-gambit.scm
@@ -1,9 +1,8 @@
 (define get-vector
   (c-lambda () scheme-object "
-    ___SCMOBJ v;
     size_t n = 32;
     size_t i;
-    v = ___make_vector(___PSTATE, n, ___FAL);
+    ___SCMOBJ v = ___make_vector(___PSTATE, n, ___FAL);
     for (i = 0; i < n; i++) {
         ___VECTORSET(v, ___FIX(i), ___FIX((i % 2) ? -i : i));
     }
